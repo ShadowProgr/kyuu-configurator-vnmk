8874428ddd6d3be1a5290ec67d096c0e812ae4f8
Hoàng Vương <14125835+ShadowProgr@users.noreply.github.com>
2021-09-24 14:59:21 +0700

    Reorganize and optimize selection change events

--- script.js
+++ script.js
@@ -215,179 +215,250 @@ function getConfigString() {
     return configString;
 };
 
+// Selection change events
+function onGrpLayoutChange() {
+    const id = $("input[type='radio'][name='grp-layout']:checked").attr("id");
+
+    if (id.includes("crane") || id.includes("flower")) {
+        $(".grp-case-material span:not(.case-alu)").fadeOut("fast");
+        $(".grp-badge-material span:not(.crane)").fadeOut("fast");
+        $(".grp-badge-color span:not(.crane)").fadeOut("fast");
+    } else {
+        $(".grp-case-material .case-pc").fadeIn("fast");
+        $(".grp-badge-material .kyuu").fadeIn("fast");
+        if (id.includes("wkl")) {
+            $(".grp-case-material .case-copper").fadeOut("fast");
+        } else {
+            $(".grp-case-material .case-copper").fadeIn("fast");
+        }
+    }
+
+    // Check if selected Case material is still valid, if not select first valid option
+    const selectedSpan = $("input[type='radio'][name='grp-case-material']:checked").parent();
+    selectedSpan.parent().children().promise().done(function () {
+        if (selectedSpan.filter(":hidden").length > 0) {
+            $("input[type='radio'][name='grp-case-material']").parent().filter(":visible").first().find("input").prop("checked", true).change();
+        }
+    });
+
+    updateRenders();
+    updatePrice();
+};
+
+function onGrpCaseMaterialChange() {
+    const id = $("input[type='radio'][name='grp-case-material']:checked").attr("id");
+
+    if (id.includes("alu")) {
+        $(".grp-case-color span:not(.case-alu-color)").fadeOut("fast");
+        $(".grp-case-color .case-alu-color").fadeIn("fast");
+    } else if (id.includes("pc")) {
+        $(".grp-case-color span:not(.case-pc-color)").fadeOut("fast");
+        $(".grp-case-color .case-pc-color").fadeIn("fast");
+    } else {
+        $(".grp-case-color span:not(.case-copper-color)").fadeOut("fast");
+        $(".grp-case-color .case-copper-color").fadeIn("fast");
+    }
+
+    // Check if selected Case color is still valid, if not select first valid option
+    const selectedSpan = $("input[type='radio'][name='grp-case-color']:checked").parent();
+    selectedSpan.parent().children().promise().done(function () {
+        if (selectedSpan.filter(":hidden").length > 0) {
+            $("input[type='radio'][name='grp-case-color']").parent().filter(":visible").first().find("input").prop("checked", true).change();
+        }
+    });
+
+    updateRenders();
+    updatePrice();
+};
+
+function onGrpBadgeMaterialChange() {
+    const id = $("input[type='radio'][name='grp-badge-material']:checked").attr("id");
+
+    if (id.includes("alu")) {
+        $(".grp-badge-color span:not(.badge-alu-color)").fadeOut("fast");
+        $(".grp-badge-color .badge-alu-color").fadeIn("fast");
+    } else if (id.includes("pc")) {
+        $(".grp-badge-color span:not(.badge-pc-color)").fadeOut("fast");
+        $(".grp-badge-color .badge-pc-color").fadeIn("fast");
+    } else if (id.includes("brass")) {
+        const layout = $("input[type='radio'][name='grp-layout']:checked").attr("id");
+        if (layout.includes("crane") || layout.includes("flower")) {
+            $(".grp-badge-color .crane").fadeIn("fast");
+        } else {
+            $(".grp-badge-color .badge-brass-color").fadeIn("fast");
+        }
+        $(".grp-badge-color span:not(.badge-brass-color)").fadeOut("fast");
+    } else if (id.includes("copper")) {
+        $(".grp-badge-color span:not(.badge-copper-color)").fadeOut("fast");
+        $(".grp-badge-color .badge-copper-color").fadeIn("fast");
+    } else {
+        $(".grp-badge-color span:not(.badge-ss-color)").fadeOut("fast");
+        $(".grp-badge-color .badge-ss-color").fadeIn("fast");
+    }
+
+    // Check if selected Badge color is still valid, if not select first valid option
+    const selectedSpan = $("input[type='radio'][name='grp-badge-color']:checked").parent();
+    selectedSpan.parent().children().promise().done(function () {
+        if (selectedSpan.filter(":hidden").length > 0) {
+            $("input[type='radio'][name='grp-badge-color']").parent().filter(":visible").first().find("input").prop("checked", true).change();
+        }
+    });
+
+    updateRenders();
+    updatePrice();
+};
+
+function onGrpWeightStyleChange() {
+    const id = $("input[type='radio'][name='grp-weight-style']:checked").attr("id");
+
+    if (id == "weight-regular") {
+        $(".weight-hybrid-color").fadeOut("fast");
+        $(".cat-subweight").fadeOut("fast");
+    } else {
+        $(".cat-subweight").fadeIn("fast");
+        if ($("input[type='radio'][name='grp-weight-material']:checked").length && $("input[type='radio'][name='grp-weight-material']:checked").attr("id").includes("brass")) {
+            $(".weight-hybrid-color").fadeIn("fast");
+        }
+    }
+
+    // Check if selected Weight color is still valid, if not select first valid option
+    const selectedSpan = $("input[type='radio'][name='grp-weight-color']:checked").parent();
+    selectedSpan.parent().children().promise().done(function () {
+        if (selectedSpan.filter(":hidden").length > 0) {
+            $("input[type='radio'][name='grp-weight-color']").parent().filter(":visible").first().find("input").prop("checked", true).change();
+        }
+    });
+
+    updateRenders();
+    updatePrice();
+};
+
+function onGrpWeightMaterialChange() {
+    const id = $("input[type='radio'][name='grp-weight-material']:checked").attr("id");
+
+    if (id.includes("alu")) {
+        $(".grp-weight-color span:not(.weight-alu-color)").fadeOut("fast");
+        $(".grp-weight-color .weight-alu-color").fadeIn("fast");
+    } else if (id.includes("pc")) {
+        $(".grp-weight-color span:not(.weight-pc-color)").fadeOut("fast");
+        $(".grp-weight-color .weight-pc-color").fadeIn("fast");
+    } else if (id.includes("brass")) {
+        $(".grp-weight-color span:not(.weight-brass-color)").fadeOut("fast");
+        if ($("input[type='radio'][name='grp-weight-style']:checked").attr("id") == "weight-regular") {
+            $(".grp-weight-color .weight-brass-color:not(.weight-hybrid-color)").fadeIn("fast");
+        } else {
+            $(".grp-weight-color .weight-brass-color").fadeIn("fast");
+        }
+    } else if (id.includes("copper")) {
+        $(".grp-weight-color span:not(.weight-copper-color)").fadeOut("fast");
+        $(".grp-weight-color .weight-copper-color").fadeIn("fast");
+    } else {
+        $(".grp-weight-color span:not(.weight-ss-color)").fadeOut("fast");
+        $(".grp-weight-color .weight-ss-color").fadeIn("fast");
+    }
+
+    // Check if selected Weight color is still valid, if not select first valid option
+    const selectedSpan = $("input[type='radio'][name='grp-weight-color']:checked").parent();
+    selectedSpan.parent().children().promise().done(function () {
+        if (selectedSpan.filter(":hidden").length > 0) {
+            $("input[type='radio'][name='grp-weight-color']").parent().filter(":visible").first().find("input").prop("checked", true).change();
+        }
+    });
+
+    updateRenders();
+    updatePrice();
+};
+
+function onGrpSubweightMaterialChange() {
+
+    const id = $("input[type='radio'][name='grp-subweight-material']:checked").attr("id");
+
+    if (id.includes("alu")) {
+        $(".grp-subweight-color span:not(.subweight-alu-color)").fadeOut("fast");
+        $(".grp-subweight-color .subweight-alu-color").fadeIn("fast");
+    } else {
+        $(".grp-subweight-color span:not(.subweight-pc-color)").fadeOut("fast");
+        $(".grp-subweight-color .subweight-pc-color").fadeIn("fast");
+    }
+
+    // Check if selected Sub-weight color is still valid, if not select first valid option
+    const selectedSpan = $("input[type='radio'][name='grp-subweight-color']:checked").parent();
+    selectedSpan.parent().children().promise().done(function () {
+        if (selectedSpan.filter(":hidden").length > 0) {
+            $("input[type='radio'][name='grp-subweight-color']").parent().filter(":visible").first().find("input").prop("checked", true).change();
+        }
+    });
+
+    updateRenders();
+    updatePrice();
+};
+
+function onGrpPlateMaterialChange() {
+    const id = $("input[type='radio'][name='grp-plate-material']:checked").attr("id");
+
+    if (id.includes("alu")) {
+        $(".grp-plate-color span:not(.plate-alu-color)").fadeOut("fast");
+        $(".grp-plate-color .plate-alu-color").fadeIn("fast");
+    } else if (id.includes("pc")) {
+        $(".grp-plate-color span:not(.plate-pc-color)").fadeOut("fast");
+        $(".grp-plate-color .plate-pc-color").fadeIn("fast");
+    } else if (id.includes("brass")) {
+        $(".grp-plate-color span:not(.plate-brass-color)").fadeOut("fast");
+        $(".grp-plate-color .plate-brass-color").fadeIn("fast");
+    } else if (id.includes("copper")) {
+        $(".grp-plate-color span:not(.plate-copper-color)").fadeOut("fast");
+        $(".grp-plate-color .plate-copper-color").fadeIn("fast");
+    } else {
+        $(".grp-plate-color span:not(.plate-pom-color)").fadeOut("fast");
+        $(".grp-plate-color .plate-pom-color").fadeIn("fast");
+    }
+
+    // Check if selected Plate color is still valid, if not select first valid option
+    const selectedSpan = $("input[type='radio'][name='grp-plate-color']:checked").parent();
+    selectedSpan.parent().children().promise().done(function () {
+        if (selectedSpan.filter(":hidden").length > 0) {
+            $("input[type='radio'][name='grp-plate-color']").parent().filter(":visible").first().find("input").prop("checked", true).change();
+        }
+    });
+
+    updateRenders();
+    updatePrice();
+};
+
 $(document).ready(function () {
-    $("input[name='grp-layout']").change(function () {
-        const id = $("input[type='radio'][name='grp-layout']:checked").attr("id");
-
-        if (id.includes("crane") || id.includes("flower")) {
-            $(".grp-case-material span:not(.case-alu)").fadeOut("fast");
-            $(".grp-badge-material span:not(.crane)").fadeOut("fast");
-            $(".grp-badge-color span:not(.crane)").fadeOut("fast");
-        } else {
-            $(".grp-case-material .case-pc").fadeIn("fast");
-            $(".grp-badge-material .kyuu").fadeIn("fast");
-            if (id.includes("wkl")) {
-                $(".grp-case-material .case-copper").fadeOut("fast");
-            } else {
-                $(".grp-case-material .case-copper").fadeIn("fast");
-            }
-        }
-        updateRenders();
-        updatePrice();
-    });
-
-    $("input[name='grp-case-material']").change(function () {
-        const id = $("input[type='radio'][name='grp-case-material']:checked").attr("id");
-
-        if (id.includes("alu")) {
-            $(".grp-case-color span:not(.case-alu-color)").fadeOut("fast");
-            $(".grp-case-color .case-alu-color").fadeIn("fast");
-        } else if (id.includes("pc")) {
-            $(".grp-case-color span:not(.case-pc-color)").fadeOut("fast");
-            $(".grp-case-color .case-pc-color").fadeIn("fast");
-        } else {
-            $(".grp-case-color span:not(.case-copper-color)").fadeOut("fast");
-            $(".grp-case-color .case-copper-color").fadeIn("fast");
-        }
-        updateRenders();
-        updatePrice();
-    });
-
+    $("input[name='grp-layout']").change(onGrpLayoutChange);
+    $("input[name='grp-case-material']").change(onGrpCaseMaterialChange);
     $("input[name='grp-case-color']").change(function () {
         updateRenders();
         updatePrice();
     });
 
-    $("input[name='grp-badge-material']").change(function () {
-        const id = $("input[type='radio'][name='grp-badge-material']:checked").attr("id");
-
-        if (id.includes("alu")) {
-            $(".grp-badge-color span:not(.badge-alu-color)").fadeOut("fast");
-            $(".grp-badge-color .badge-alu-color").fadeIn("fast");
-        } else if (id.includes("pc")) {
-            $(".grp-badge-color span:not(.badge-pc-color)").fadeOut("fast");
-            $(".grp-badge-color .badge-pc-color").fadeIn("fast");
-        } else if (id.includes("brass")) {
-            const layout = $("input[type='radio'][name='grp-layout']:checked").attr("id");
-            if (layout.includes("crane") || layout.includes("flower")) {
-                $(".grp-badge-color .crane").fadeIn("fast");
-            } else {
-                $(".grp-badge-color .badge-brass-color").fadeIn("fast");
-            }
-            $(".grp-badge-color span:not(.badge-brass-color)").fadeOut("fast");
-        } else if (id.includes("copper")) {
-            $(".grp-badge-color span:not(.badge-copper-color)").fadeOut("fast");
-            $(".grp-badge-color .badge-copper-color").fadeIn("fast");
-        } else {
-            $(".grp-badge-color span:not(.badge-ss-color)").fadeOut("fast");
-            $(".grp-badge-color .badge-ss-color").fadeIn("fast");
-        }
-        updateRenders();
-        updatePrice();
-    });
-
+    $("input[name='grp-badge-material']").change(onGrpBadgeMaterialChange);
     $("input[name='grp-badge-color']").change(function () {
         updateRenders();
         updatePrice();
     });
 
-    $("input[name='grp-weight-style']").change(function () {
-        const id = $("input[type='radio'][name='grp-weight-style']:checked").attr("id");
-
-        if (id == "weight-regular") {
-            $(".cat-subweight").fadeOut("fast");
-            $(".weight-hybrid-color").fadeOut("fast");
-        } else {
-            $(".cat-subweight").fadeIn("fast");
-            if ($("input[type='radio'][name='grp-weight-material']:checked").length && $("input[type='radio'][name='grp-weight-material']:checked").attr("id").includes("brass")) {
-                $(".weight-hybrid-color").fadeIn("fast");
-            }
-        }
-        updateRenders();
-        updatePrice();
-    });
-
-    $("input[name='grp-weight-material']").change(function () {
-        const id = $("input[type='radio'][name='grp-weight-material']:checked").attr("id");
-
-        if (id.includes("alu")) {
-            $(".grp-weight-color span:not(.weight-alu-color)").fadeOut("fast");
-            $(".grp-weight-color .weight-alu-color").fadeIn("fast");
-        } else if (id.includes("pc")) {
-            $(".grp-weight-color span:not(.weight-pc-color)").fadeOut("fast");
-            $(".grp-weight-color .weight-pc-color").fadeIn("fast");
-        } else if (id.includes("brass")) {
-            $(".grp-weight-color span:not(.weight-brass-color)").fadeOut("fast");
-            if ($("input[type='radio'][name='grp-weight-style']:checked").attr("id") == "weight-regular") {
-                $(".grp-weight-color .weight-brass-color:not(.weight-hybrid-color)").fadeIn("fast");
-            } else {
-                $(".grp-weight-color .weight-brass-color").fadeIn("fast");
-            }
-        } else if (id.includes("copper")) {
-            $(".grp-weight-color span:not(.weight-copper-color)").fadeOut("fast");
-            $(".grp-weight-color .weight-copper-color").fadeIn("fast");
-        } else {
-            $(".grp-weight-color span:not(.weight-ss-color)").fadeOut("fast");
-            $(".grp-weight-color .weight-ss-color").fadeIn("fast");
-        }
-        updateRenders();
-        updatePrice();
-    });
-
+    $("input[name='grp-weight-style']").change(onGrpWeightStyleChange);
+    $("input[name='grp-weight-material']").change(onGrpWeightMaterialChange);
     $("input[name='grp-weight-color']").change(function () {
         updateRenders();
         updatePrice();
     });
 
-    $("input[name='grp-subweight-material']").change(function () {
-        const id = $("input[type='radio'][name='grp-subweight-material']:checked").attr("id");
-
-        if (id.includes("alu")) {
-            $(".grp-subweight-color span:not(.subweight-alu-color)").fadeOut("fast");
-            $(".grp-subweight-color .subweight-alu-color").fadeIn("fast");
-        } else {
-            $(".grp-subweight-color span:not(.subweight-pc-color)").fadeOut("fast");
-            $(".grp-subweight-color .subweight-pc-color").fadeIn("fast");
-        }
-        updateRenders();
-        updatePrice();
-    });
-
+    $("input[name='grp-subweight-material']").change(onGrpSubweightMaterialChange);
     $("input[name='grp-subweight-color']").change(function () {
         updateRenders();
         updatePrice();
     });
 
-    $("input[name='grp-plate-material']").change(function () {
-        const id = $("input[type='radio'][name='grp-plate-material']:checked").attr("id");
-
-        if (id.includes("alu")) {
-            $(".grp-plate-color span:not(.plate-alu-color)").fadeOut("fast");
-            $(".grp-plate-color .plate-alu-color").fadeIn("fast");
-        } else if (id.includes("pc")) {
-            $(".grp-plate-color span:not(.plate-pc-color)").fadeOut("fast");
-            $(".grp-plate-color .plate-pc-color").fadeIn("fast");
-        } else if (id.includes("brass")) {
-            $(".grp-plate-color span:not(.plate-brass-color)").fadeOut("fast");
-            $(".grp-plate-color .plate-brass-color").fadeIn("fast");
-        } else if (id.includes("copper")) {
-            $(".grp-plate-color span:not(.plate-copper-color)").fadeOut("fast");
-            $(".grp-plate-color .plate-copper-color").fadeIn("fast");
-        } else {
-            $(".grp-plate-color span:not(.plate-pom-color)").fadeOut("fast");
-            $(".grp-plate-color .plate-pom-color").fadeIn("fast");
-        }
-        updateRenders();
-        updatePrice();
-    });
-
+    $("input[name='grp-plate-material']").change(onGrpPlateMaterialChange);
     $("input[name='grp-plate-color']").change(function () {
         updateRenders();
         updatePrice();
     });
 
     $("input[name='grp-pcb']").change(function () {
-        updateRenders();
         updatePrice();
     });
 
