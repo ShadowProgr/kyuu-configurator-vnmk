5472b645337327caf3c6fa46372be059770a170c
Hoàng Vương <14125835+ShadowProgr@users.noreply.github.com>
2021-09-29 14:08:37 +0700

    Fix price type in form to int

--- script.js
+++ script.js
@@ -590,7 +590,7 @@ $(document).ready(function () {
                     '</div>')
             });
 
-            $("#price").val($(".price-number").text());
+            $("#price").val(prices.reduce((a, b) => a + b));
             $("#config").val(configs);
             $("#form").fadeIn("fast");
         } else {
